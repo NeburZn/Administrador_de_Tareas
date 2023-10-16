4c06784f82ec5f01c139f58062a6b29958f49acf
nebur <rubenarranz04@gmail.com>
2023-10-16 17:29:42 +0200

    Initial commit

---
+++ app/src/main/java/com/example/administradordetareas/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.administradordetareas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.administradordetareas.ui.theme.AdministradorDeTareasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AdministradorDeTareasTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Contenido(texto1 = stringResource(R.string.texto1), texto2 = stringResource(R.string.texto2))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun  Contenido (texto1: String, texto2: String){
+    Column (verticalArrangement = Arrangement.Center){
+        val image = painterResource(R.drawable.ic_task_completed)
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = texto1,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = texto2,
+            fontSize = 16.sp,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AdministradorDeTareasTheme {
+        Contenido(texto1 = stringResource(R.string.texto1), texto2 = stringResource(R.string.texto2))
+    }
+}
